753ba68d46219f73953096d4210f67268f87b173
Shmel <shmel71rus1@gmail.com>
2024-08-15 16:06:01 +0300

    added base keyboard

--- app/handlers.py
+++ app/handlers.py
@@ -1,13 +1,14 @@
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram import F, Router
+import app.keyboards as kb
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(f'Привет {message.from_user.first_name}')
+    await message.answer(f'Привет {message.from_user.first_name}', reply_markup=kb.main_keyboard)
     print(message)
 
 
